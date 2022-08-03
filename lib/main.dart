04c9e731c9f1bfd01921d08398521ef28392818f
Francis Rodrigues <francis.rod01@outlook.com>
2022-08-03 00:59:01 -0300

    feat(main screen): Defined home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:payment_dashboard/components/dashboard.dart';
+import 'package:payment_dashboard/components/sidebar.dart';
 import 'package:payment_dashboard/constants.dart';
 
 void main() {
@@ -15,6 +17,31 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
         canvasColor: bgColor,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+            Expanded(
+              child: SideBar(),
+            ),
+            Expanded(
+              flex: 5,
+              child: Dashboard(),
+            ),
+          ],
+        ),
       ),
     );
   }
