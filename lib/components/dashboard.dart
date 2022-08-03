43e7754d1f851bade9830fac1d26df9039628b1c
Francis Rodrigues <francis.rod01@outlook.com>
2022-08-03 01:01:08 -0300

    feat(components): Defined the dashboard

---
+++ lib/components/dashboard.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Dashboard extends StatelessWidget {
+  const Dashboard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {}
+}
