f1ec2987fd1802b58a8439b2a8456619e8d5ad5c
Francis Rodrigues <francis.rod01@outlook.com>
2022-08-03 01:04:38 -0300

    feat(constants): Added some colors and padding

---
+++ lib/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const blackColor = Color(0xFF14111D);
+const secondaryColor = Color(0xFF2A2D3E);
+const bgColor = Color(0xFF212332);
+
+const defaultPadding = 20.0;
