409f51708fa40388199a98c43e9b5b5584abcab0
Francis Rodrigues <francis.rod01@outlook.com>
2022-08-03 22:06:47 -0300

    feat(components): Added first lines of dashboard

--- lib/components/dashboard.dart
+++ lib/components/dashboard.dart
@@ -1,8 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:payment_dashboard/constants.dart';
 
 class Dashboard extends StatelessWidget {
   const Dashboard({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {}
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(defaultPadding),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  const Text(
+                    'Dashboard',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 25,
+                    ),
+                  ),
+                  const Spacer(flex: 2),
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Search',
+                        hintStyle: const TextStyle(color: Colors.white70),
+                        fillColor: secondaryColor,
+                        filled: true,
+                        border: const OutlineInputBorder(
+                          borderSide: BorderSide.none,
+                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                        ),
+                        suffixIcon: InkWell(
+                          onTap: () {},
+                          child: Container(
+                            padding: const EdgeInsets.all(
+                              defaultPadding * 0.75,
+                            ),
+                            margin: const EdgeInsets.symmetric(
+                              horizontal: defaultPadding / 2,
+                            ),
+                            decoration: const BoxDecoration(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(10),
+                              ),
+                            ),
+                            child: const Icon(
+                              Icons.search,
+                              color: Colors.white70,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
